c6cd950d288f0d24a89e1a5880716ea56ed16ed2
cswelch <colewelch151@gmail.com>
2026-09-17 11:39:36 -0400

    Added pacedown test to recovery_iteration.py and made parameters more uniform with SINDy_non_autonomous_test.ipynb.

--- recovery_iteration.py
+++ recovery_iteration.py
@@ -23,7 +23,7 @@ from fhn_models import fhn, fhn_c, fhn_vf_4, fhn_vf_7, compare_exact_and_sindy_c
 #   period (float): The period of the stimulus
 #   dur (float): The time duration of the stimulus
 #   mag (float): The magnitude of the stimulus
-def stimulus(t):
+def func_log(t):
     period=155.0
     dur=5.0
     mag=0.12
@@ -31,14 +31,61 @@ def stimulus(t):
     stimulus = mag * (np.mod(t, period) <= dur)
     return stimulus
 
-dt = 0.1    # Time step
+'''
+    Function that defines stimulus with 3 sets of stim_count pulses, each with decreasing period.
+    Params:
+        t (array): Time values
+        dur (float): Duration of each pulse
+        mag (float): Magnitude of the stimulus
+        stim_count (int): Number of stimuli of each period to generate
+        periods (array): Array of periods for each set of pulses
+'''
+def func_pacedown(t):
+    dur=5.0
+    mag=0.12
+    stim_count=3
+    periods = np.array([305., 230., 155.])
+    
+    period_0_multiple = np.minimum(np.floor(t / periods[0]), stim_count)
+    period_1_multiple = np.minimum(np.floor((t - period_0_multiple * periods[0]) / periods[1]), stim_count)
+    period_2_multiple = np.minimum(np.floor((t - period_0_multiple * periods[0] - period_1_multiple * periods[1]) / periods[2]), stim_count)
+
+    local_t_0 = t
+    local_t_1 = t - stim_count * periods[0]
+    local_t_2 = t - stim_count * periods[0] - stim_count * periods[1]
+
+    is_stim = np.zeros_like(t, dtype=bool)
+
+    mask_0 = period_0_multiple < stim_count
+    mask_1 = (~mask_0) & (period_1_multiple < stim_count)
+    mask_2 = (~mask_0) & (~mask_1) & (period_2_multiple < stim_count)
+
+    stim_0 = np.mod(local_t_0, periods[0]) <= dur
+    stim_1 = np.mod(local_t_1, periods[1]) <= dur
+    stim_2 = np.mod(local_t_2, periods[2]) <= dur
+
+    # Use np.where to allow both scalar and array t to work.
+    is_stim = np.where(
+        mask_0,
+        stim_0,
+        np.where(mask_1,
+                 stim_1,
+                 np.where(mask_2,
+                          stim_2,
+                          False))
+    )
+
+    stimulus = mag * is_stim
+    return stimulus
+
+dt = 0.05    # Time step
 root_find_rel_tol = 1e-3 # Relative tolerance for Brent's method application (starts to fail around 0.5–1)
 t_end_sim = 2000    # Upper bound of integration
-t_end_vis = 400     # Upper bound of x-axis on plots
+t_end_vis = 2000     # Upper bound of x-axis on plots
 n = int(t_end_sim / dt)   # Number of time steps
 t_fhn = np.arange(0, t_end_sim, dt)    # Time range for integration
 x_0_fhn = np.array([0, 0])   # ICs
-states_fhn = odeint(fhn, x_0_fhn, t_fhn, args=(stimulus,), hmax=0.01) # Real n x 2 reference matrix of [u, v]
+states_fhn = odeint(fhn, x_0_fhn, t_fhn, args=(func_pacedown,), hmax=0.01) # Real n x 2 reference matrix of [u, v]
 
 # Start w/ initial recovery variable value
 v_old_est = 0
@@ -54,7 +101,7 @@ for i in range(n-1):
     def voltage_error(v_candidate):
         ics_cur = np.array([u_old, v_candidate])
         t_cur = np.array([t_old, t_new])
-        out = odeint(fhn, ics_cur, t_cur, args=(stimulus,), hmax=0.01)
+        out = odeint(fhn, ics_cur, t_cur, args=(func_pacedown,), hmax=0.01)
         return out[1, 0] - u_new
 
     lower = v_old_est - 0.05
@@ -96,8 +143,8 @@ v_estimated = np.asarray(estimated_vs)
 assert len(t_fit) == len(u_fit) == len(v_estimated)
 
 gen_library_fhn = GenLibraryFit(
-    stimulus,
-    stimulus,
+    func_pacedown,
+    func_pacedown,
     fhn_variant="standard",
     t_range=t_fit,
     ics=x_0_fhn,
